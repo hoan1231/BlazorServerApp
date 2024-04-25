04a6c169ff631f418f968d76e1544fc9e53628d3
hoanlv <lamhoan96@gmail.com>
2024-04-25 17:31:39 +0700

    h

--- BlazorServerApp/Pages/Login.razor.cs
+++ BlazorServerApp/Pages/Login.razor.cs
@@ -18,9 +18,9 @@ namespace BlazorServerApp.Pages
         [Inject] public ILocalStorageService _localStorageService { get; set; }
         [Inject] private IPackageBzService packageBzService { get; set; }
 
-        public LoginModel login = new LoginModel { UserName="",PassWord="11" };
+        public LoginModel login = new LoginModel { UserName="",PassWord="113333" };
         private bool loading = false;
-        private string Error = "333";
+        private string Error = "66666";
         async Task LoginUser(EditContext editContext)
         {
             loading = true;
